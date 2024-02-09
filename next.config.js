19d26e2bc9f76e87765fc81f2cdc8b498e98b0ff
Samin Farajian <farajiansamin@gmail.com>
2024-02-09 15:46:30 -0700

    updated

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = { reactStrictMode: true,
+  
+    // Add basePath
+     basePath: '/github-pages',}
 
 module.exports = nextConfig
